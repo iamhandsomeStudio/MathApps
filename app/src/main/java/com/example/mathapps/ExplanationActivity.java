0745b9f3658630add6da8b83b7e4f388e9618c06
willy <dq6e5g@gmail.com>
2021-12-07 22:11:36 +0800

    增加說明圖片的放置

---
+++ app/src/main/java/com/example/mathapps/ExplanationActivity.java
@@ -0,0 +1,15 @@
+package com.example.mathapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExplanationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explanation);
+
+    }
+}
