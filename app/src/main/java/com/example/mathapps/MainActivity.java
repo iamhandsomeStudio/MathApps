6a59b0d07022770b983091bb3fbf84b555faa74d
an55664 <an665510@gmail.com>
2021-12-08 01:27:02 +0800

    遊戲程式修改

--- app/src/main/java/com/example/mathapps/MainActivity.java
+++ app/src/main/java/com/example/mathapps/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mathapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.renderscript.Sampler;
 import android.util.Log;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     TextView que1,que2,que3,que4,que5,que6;
     int[] x = new int[12];
     int[] y = new int[6];
-    int sum2,count = 0;
+    int sum2,count = 0,score = 0;
     String[] ans = new String[6];
 
 
@@ -44,21 +46,7 @@ public class MainActivity extends AppCompatActivity {
         btn11 = (Button)findViewById(R.id.elevenButton);
         btn12 = (Button)findViewById(R.id.twelveButton);
 
-
-
-        btn.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn2.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn3.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn4.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn5.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn6.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn7.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn8.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn9.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn10.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn11.setText(String.valueOf((int)(Math.random()*10+1)));
-        btn12.setText(String.valueOf((int)(Math.random()*10+1)));
-
+        btnText();
 
         for(int i=0;i<6;i++){
             y[i] = (int)(Math.random()*20+1);
@@ -78,94 +66,163 @@ public class MainActivity extends AppCompatActivity {
         que5.setText(String.valueOf(y[4]));
         que6.setText(String.valueOf(y[5]));
 
+
     }
 
     public void click(View v){
+        countGet();
         x[0] = Integer.parseInt(String.valueOf(btn.getText()));
+        btn.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[0]);
-
     }
     public void click_2(View v){
+        countGet();
         x[1] = Integer.parseInt(String.valueOf(btn2.getText()));
+        btn2.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[1]);
     }
     public void click_3(View v){
+        countGet();
         x[2] = Integer.parseInt(String.valueOf(btn3.getText()));
+        btn3.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[2]);
     }
     public void click_4(View v){
+        countGet();
         x[3] = Integer.parseInt(String.valueOf(btn4.getText()));
+        btn4.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[3]);
     }
     public void click_5(View v){
+        countGet();
         x[4] = Integer.parseInt(String.valueOf(btn5.getText()));
+        btn5.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[4]);
     }
     public void click_6(View v){
+        countGet();
         x[5] = Integer.parseInt(String.valueOf(btn6.getText()));
+        btn6.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[5]);
     }
     public void click_7(View v){
+        countGet();
         x[6] = Integer.parseInt(String.valueOf(btn7.getText()));
+        btn7.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[6]);
     }
     public void click_8(View v){
+        countGet();
         x[7] = Integer.parseInt(String.valueOf(btn8.getText()));
+        btn8.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[7]);
     }
     public void click_9(View v){
+        countGet();
         x[8] = Integer.parseInt(String.valueOf(btn9.getText()));
+        btn9.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[8]);
     }
     public void click_10(View v){
+        countGet();
         x[9] = Integer.parseInt(String.valueOf(btn10.getText()));
+        btn10.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[9]);
     }
     public void click_11(View v){
+        countGet();
         x[10] = Integer.parseInt(String.valueOf(btn11.getText()));
+        btn11.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[10]);
     }
     public void click_12(View v){
+        countGet();
         x[11] = Integer.parseInt(String.valueOf(btn12.getText()));
+        btn12.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
         sum(x[11]);
     }
 
     public void sum(int a){
-        count++;
         sum2 += a;
-        System.out.println(y[0]);
-        System.out.println(y[1]);
-        System.out.println(y[2]);
-        System.out.println(y[3]);
-        System.out.println(y[4]);
-        System.out.println(y[5]);
+        System.out.println("總和:" + sum2 + "次數:" + count);
         if(y[0] == sum2 && count == 2){
             que1.setText("完成");
+            score += 10;
             sum2 = 0;
             count = 0;
+            btnColor();
         }else if(y[1] == sum2 && count == 2){
             que2.setText("完成");
+            score += 10;
             sum2 = 0;
             count = 0;
+            btnColor();
         }else if(y[2] == sum2 && count == 2){
             que3.setText("完成");
+            score += 10;
             sum2 = 0;
             count = 0;
+            btnColor();
         }else if(y[3] == sum2 && count == 2){
             que4.setText("完成");
+            score += 10;
             sum2 = 0;
             count = 0;
+            btnColor();
         }else if(y[4] == sum2 && count == 2){
             que5.setText("完成");
+            score += 10;
             sum2 = 0;
             count = 0;
+            btnColor();
         }else if(y[5] == sum2 && count == 2){
             que6.setText("完成");
+            score += 10;
             sum2 = 0;
             count = 0;
+            btnColor();
         }else if(count == 2){
             sum2 = 0;
             count = 0;
+            btnColor();
         }
+    }
+
+    public void btnColor(){
+        btn.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn2.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn3.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn4.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn5.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn6.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn7.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn8.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn9.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn10.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn11.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+        btn12.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
+    }
+
+    public void btnText(){
+        btn.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn2.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn3.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn4.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn5.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn6.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn7.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn8.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn9.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn10.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn11.setText(String.valueOf((int)(Math.random()*10+1)));
+        btn12.setText(String.valueOf((int)(Math.random()*10+1)));
+    }
+
+    public void countGet(){
+        count++;
+        if(count > 2){
+            count = 0;
+        }
+        System.out.println(count);
     }
 }
