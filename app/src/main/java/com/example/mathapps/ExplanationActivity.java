c6b187c882d5fd6514475b6dfd7dfd375a33d410
willy <dq6e5g@gmail.com>
2021-12-10 23:49:47 +0800

    ExplanationActivity 增加intent換頁的功能以及進入welcome畫面啟動

--- app/src/main/java/com/example/mathapps/ExplanationActivity.java
+++ app/src/main/java/com/example/mathapps/ExplanationActivity.java
@@ -2,7 +2,10 @@ package com.example.mathapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ExplanationActivity extends AppCompatActivity {
 
@@ -10,6 +13,14 @@ public class ExplanationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explanation);
+        Button nextButton = findViewById(R.id.button);
+        nextButton.setOnClickListener(this::next_onClick);
 
+    }
+
+    public void next_onClick(View v)
+    {
+        Intent nextIntnet = new Intent(ExplanationActivity.this, SelectActivity.class);
+        startActivity(nextIntnet);
     }
 }
--- app/src/main/java/com/example/mathapps/WelcomeActivity.java
+++ app/src/main/java/com/example/mathapps/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.mathapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(() -> {
+            Intent startLandingPageActivity = new Intent(WelcomeActivity.this, ExplanationActivity.class);
+            startActivity(startLandingPageActivity);
+            },5000);
     }
 }
