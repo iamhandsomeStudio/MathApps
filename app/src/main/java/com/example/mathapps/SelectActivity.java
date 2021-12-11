d93642f179dbe559b8003523972e9134d3ea74c6
willy <dq6e5g@gmail.com>
2021-12-11 23:27:21 +0800

    增加隨機英文姓名

--- app/src/main/java/com/example/mathapps/SelectActivity.java
+++ app/src/main/java/com/example/mathapps/SelectActivity.java
@@ -7,13 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.Random;
 
 public class SelectActivity extends AppCompatActivity {
 
     Button start;
     Spinner dif_spinner,ex_spinner;
     ArrayAdapter<CharSequence> dif_adapter,ex_adapter;
+    EditText nameEditText;
+    String[] names;
+    Random nameNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +39,33 @@ public class SelectActivity extends AppCompatActivity {
         ex_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         ex_spinner.setAdapter(ex_adapter);
 
+        nameEditText = (EditText)findViewById(R.id.editTextTextPersonName);
+        names = new String[]{
+                "Aaron", "Abel", "Abraham", "Adam",
+                "Adrian", "Aidan", "Alva", "Alex",
+                "Alan", "Albert", "Ben", "Benjamin",
+                "Bert", "Benson", "Bill", "Billy",
+                "Blake", "Bob", "Bobby", "Brad",
+                "Brandon", "Brant", "Brent", "Brian",
+                "Caleb", "Cameron", "Carl", "Carlos",
+                "Cary", "Caspar", "Cecil", "Charles",
+                "Chris", "Christian", "Clark", "Cliff",
+                "Daniel", "Denny", "Darwin", "David",
+                "Eddie", "Edwin", "Eric", "Evan",
+                "Fred","Frank", "Gino", "Jack",
+                "Jackson", "Jacob", "James", "Jason",
+                "Jimmy", "Joe", "John", "Lee", "Leo",
+                "Max", "Michael", "Mickey", "Mike",
+                "Sonia", "Sophia"};
+
+        nameNumber = new Random();
+        System.out.println(names[nameNumber.nextInt(62)]);
+        nameEditText.setText(names[nameNumber.nextInt(62)]);
     }
 
     public void start(View v){
         Intent intent = new Intent(SelectActivity.this,MainActivity.class);
+        intent.putExtra("name", nameEditText.getText());
         intent.putExtra("dif",dif_spinner.getSelectedItem().toString());
         intent.putExtra("ex",ex_spinner.getSelectedItem().toString());
         startActivity(intent);
